545a13ee39f26c8515b851d1eb6702336e56150a
Farhan <66604627+wahidfarhan@users.noreply.github.com>
2025-07-01 19:14:40 +0600

    Create  1007 - Difference.c

---
+++ Beecrowd problems/BEGINNER/1007 - Difference.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,SOMA,c,d;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    scanf("%d",&d);
+    SOMA = (a*b)-(c*d);
+    printf("DIFERENCA = %d\n",SOMA);
+    return 0;
+}
